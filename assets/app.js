f295f7d160468364c494f08627958570fd4c6582
Virgile Mendes <mendes.virgile@gmail.com>
2020-09-30 13:46:48 +0200

    tp1
    HTML 0.1
    - création d'une image svg avec une ligne de case et une ligne de bordure
    
    CSS 0.1
    - définition de 2 couleurs pour les cases et pour les bordures/intersections
    
    JS 0.1
    - création du fichier et d'un commentaire sur l'attribut data
    
    ARBORESCENCE
    - créations d'un index.html et d'un dossier assets avec un app.js et un styles.css

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+/* for (let i=0; i<3; i=i+1) {
+    $("div").append("<p>Hop</p>");
+} */
+
+/* Avec l'attribut data, on peut stocker ce qu'on veut. Par exemple les données x,y des cases et valider ou non l'action de se déplacer d'une case (x=x y=y+1 -> valide / x=x y=y-2 -> erreur) */
