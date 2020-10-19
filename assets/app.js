ac07351f9ac2de861e2bd4bf8ab7a4c8969b98ce
Virgile Mendes <mendes.virgile@gmail.com>
2020-10-19 10:58:00 +0200

    tp3
    CSS 0.11
    - définition de couleurs pour les 2 joueurs
    
    JS 0.3
    - Ajout dans la fonction generateurPlateau la génération des pions de départ
    - Création de la fonction generateurCircle qui créer des rectangles SVG selon les coordonnées et la place souhaitée. Elle comprend aussi 2 données : la class CSS du joueur actif et les coordonnées x,y dans la propriété data.

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,9 @@
-/* Avec l'attribut data, on peut stocker ce qu'on veut. Par exemple les données x,y des cases et valider ou non l'action de se déplacer d'une case (x=x y=y+1 -> valide / x=x y=y-2 -> erreur) */
-
 "use strict"
 
+/*  $('svg').html($('svg').html());
+    --> permet d'actualiser le SVG mais ceci enlève le .on("click").
+    Solution = pointer le .on("click") sur le body et son enfant
+    --> ('body').on('.case' "click") */
 
 $(document).ready(function() {
         
@@ -20,7 +22,26 @@ $(document).ready(function() {
 
 
 
+/* Déplacement des pions */
 
+    let tourJoueur = 1;
+
+    $('.case').on("click", function () {
+
+        $(`.joueur${tourJoueur}`).attr('cx', parseInt($(this).attr('x'))+widthCase/2);
+        $(`.joueur${tourJoueur}`).attr('cy', parseInt($(this).attr('y'))+widthCase/2);
+
+
+        /* changement de joueur */
+        if (tourJoueur == 1 ) {
+            tourJoueur++;
+        } else {
+            tourJoueur = 1;
+        }
+
+        
+
+    });
 
 
 
@@ -58,12 +79,19 @@ $(document).ready(function() {
 
     /* Générateur de rectangle SVG selon les coordonnées et les dimensions souhaitées. Elle comprend aussi 2 données : la class CSS et les coordonnées x,y dans la propriété data */
     function generateurRect (classCss,numLigne,numColonne,positionX,positionY,largeur,hauteur) {
-        let rect = `<rect class="${classCss}" data="${numLigne},${numColonne}" x="${positionX}px" y="${positionY}px" width="${largeur}" height="${hauteur}"/>`;
+        let rect = `<rect class="${classCss}" data-ligne="${numLigne}" data-colonne="${numColonne}" x="${positionX}" y="${positionY}" width="${largeur}" height="${hauteur}"/>`;
 
         return rect;
     }
 
+    /* Générateur de cercle SVG selon les coordonnées et les dimensions souhaitées. Elle comprend aussi 2 données : la class CSS du Joueur et les coordonnées x,y dans la propriété data */
+    function generateurCircle (classJoueur,numLigne,numColonne,positionX,positionY,rayon) {
+        let circle = `<circle class="${classJoueur}" data-ligne="${numLigne}" data-colonne="${numColonne}" cx="${positionX}" cy="${positionY}" r="${rayon}" />`;
 
+        
+
+        return circle;
+    }
 
 
     
@@ -75,7 +103,7 @@ $(document).ready(function() {
         for (let numLigne = 0; numLigne<9; numLigne++) {
             /* Génération des cases */
             for (let numColonne=0; numColonne<9; numColonne++) {
-                plateau = plateau+generateurRect("case",numLigne+1,numColonne+1,numColonne*(widthCase+widthBordure),numLigne*(widthCase+widthBordure),widthCase,widthCase);
+                plateau = plateau+generateurRect("case",numLigne,numColonne,numColonne*(widthCase+widthBordure),numLigne*(widthCase+widthBordure),widthCase,widthCase);
             }
             /* Génération des bordures verticales */
             for (let numColonne=0; numColonne<8; numColonne++) {
@@ -94,6 +122,13 @@ $(document).ready(function() {
                 plateau = plateau+generateurRect("intersection",numLigne,numColonne,widthCase+numColonne*(widthCase+widthBordure),widthCase+numLigne*(widthCase+widthBordure),widthBordure,widthBordure);
             }
         }
+
+        /* Génération des pions */
+
+        /* Joueur 1 */
+        plateau = plateau+generateurCircle ('joueur1',8,4,(widthCase*4+widthBordure*4+widthCase/2),(widthCase*8+widthBordure*8+widthCase/2),widthCase/2);
+        /* Joueur 2 */
+        plateau = plateau+generateurCircle ('joueur2',0,4,(widthCase*4+widthBordure*4+widthCase/2),(0+widthCase/2),widthCase/2);
 
 
         return plateau;
