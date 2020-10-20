2f96a2f48de17f08b165892b2a24ee5e3232a020
Virgile Mendes <mendes.virgile@gmail.com>
2020-10-20 22:15:29 +0200

    tp3 + 1h30
    JS 0.3
    - Création de la fonction valideDeplacelement qui renvoie un booléen si oui ou non le joueur peut se deplacer sur la case souhaité.
    - Ajout de différentes données ou notions en commentaire en haut du fichier.
    - Modification de $('.case').on("click", function () en $("body").on("click", ".case", function(). Permet d'éviter la suppression du on("click") dans le SVG (voir haut du fichier).

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,17 @@
     Solution = pointer le .on("click") sur le body et son enfant
     --> ('body').on('.case' "click") */
 
+/* || --> OU ___ && --> ET */
+
+/*  console.log("data-ligne de la case : "+$(this).data(`ligne`));
+    console.log("data-ligne du joueur : "+$(`.joueur${tourJoueur}`).data(`ligne`));
+
+    console.log("data-colonne de la case : "+$(this).data(`colonne`));
+    console.log("data-colonne du joueur : "+$(`.joueur${tourJoueur}`).data(`colonne`)); */
+
+/* $("svg").html($("svg").html());
+   Lorsque on implimente des formes dans un SVG à partir d'un script, il peut il avoir des problèmes d'actualisations. Pour palier à ce problème, il faut (à chaque implimentation) réactualiser le HTML. Attention au (`élement à l'intérieur d'un SVG`).on("click") car en faisant .html cela les supprime. */
+
 $(document).ready(function() {
         
 /* Dessin du plateau */
@@ -26,11 +37,21 @@ $(document).ready(function() {
 
     let tourJoueur = 1;
 
-    $('.case').on("click", function () {
+    $("body").on("click", ".case", function(){
 
-        $(`.joueur${tourJoueur}`).attr('cx', parseInt($(this).attr('x'))+widthCase/2);
-        $(`.joueur${tourJoueur}`).attr('cy', parseInt($(this).attr('y'))+widthCase/2);
 
+        
+    if (valideDeplacement($(`#joueur${tourJoueur}`).data(`ligne`),$(`#joueur${tourJoueur}`).data(`colonne`),$(this).data(`ligne`),$(this).data(`colonne`)) == true) {
+        
+
+        $(`#joueur${tourJoueur}`).attr('cx', parseInt($(this).attr('x'))+widthCase/2);
+        $(`#joueur${tourJoueur}`).data(`ligne`, ($(this).data(`ligne`)));
+        
+        $(`#joueur${tourJoueur}`).attr('cy', parseInt($(this).attr('y'))+widthCase/2);
+        $(`#joueur${tourJoueur}`).data(`colonne`, ($(this).data(`colonne`)));
+
+
+        
 
         /* changement de joueur */
         if (tourJoueur == 1 ) {
@@ -39,7 +60,11 @@ $(document).ready(function() {
             tourJoueur = 1;
         }
 
-        
+    } else {
+        alert(`Joueur ${tourJoueur} : Vous ne pouvez pas vous déplacer sur cette case !`)
+    }
+
+    
 
     });
 
@@ -86,9 +111,7 @@ $(document).ready(function() {
 
     /* Générateur de cercle SVG selon les coordonnées et les dimensions souhaitées. Elle comprend aussi 2 données : la class CSS du Joueur et les coordonnées x,y dans la propriété data */
     function generateurCircle (classJoueur,numLigne,numColonne,positionX,positionY,rayon) {
-        let circle = `<circle class="${classJoueur}" data-ligne="${numLigne}" data-colonne="${numColonne}" cx="${positionX}" cy="${positionY}" r="${rayon}" />`;
-
-        
+        let circle = `<circle class="${classJoueur}" id="${classJoueur}" data-ligne="${numLigne}" data-colonne="${numColonne}" cx="${positionX}" cy="${positionY}" r="${rayon}" />`;
 
         return circle;
     }
@@ -137,6 +160,38 @@ $(document).ready(function() {
 
 
 
+    function valideDeplacement (ligneDepart,colonneDepart,ligneArrivee,colonneArrivee) {
+
+        /* console.log("ligneDepart : "+ligneDepart);
+        console.log("ligneArrivee : "+ligneArrivee);
+
+        console.log("colonneDepart : "+colonneDepart);
+        console.log("colonneArrivee : "+colonneArrivee); */
+        
+        let verifLigne = Boolean , verifColonne = Boolean
+        
+        verifLigne = ligneDepart == ligneArrivee;
+        
+        if (verifLigne == true) {
+            verifColonne = (colonneDepart == colonneArrivee - 1) || (colonneDepart == colonneArrivee + 1);
+        } else {
+            verifLigne = (ligneDepart == ligneArrivee - 1) || (ligneDepart == ligneArrivee + 1);
+            verifColonne = colonneDepart == colonneArrivee;
+        }
+
+        if (verifLigne == true && verifColonne == true) {
+            /* console.log(true); */
+            return true;
+        } else {
+            /* console.log(false); */
+            return false;
+        }
+
+
+    }
+
+
+    
 
 
 });
