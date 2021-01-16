71dce99a1fdeb23ff33f41c6ddd1c0c0026952b0
Virgile Mendes <mendes.virgile@gmail.com>
2021-01-16 16:07:32 +0100

    2h15
    JS 0.6
    - Ajout des zones de stockage de batonnets
    
    CSS 0.3
    - Ajout d'un container grid
    - Ajout de propriétés par rapports aux bâtonnet

--- assets/app.js
+++ assets/app.js
@@ -26,14 +26,15 @@ $(document).ready(function() {
 /* Dessin du plateau */
 
     /* Définition des largeurs des cases et des bordures */
-    let widthCase = 50;
-    let widthBordure = 10;
+    const widthCase = 50;
+    const widthBordure = 10;
 
     /* console.log(`Plateau de ${widthCase}px par case / ${widthBordure}px par bordure`); */
 
     /* Génération du plateau en SVG */
-    $("div").append(`<svg class="plateau" height="${widthCase*9+widthBordure*8}" width="${widthCase*9+widthBordure*8}">`+generateurPlateau(widthCase,widthBordure)+`</svg>`);
-
+    $(".container").append(`<svg class="stockage-2" height="${widthCase*2+widthBordure}" width="${widthCase*9+widthBordure*8}">`+generateurStockage('joueur2',widthCase,widthBordure)+`</svg>`);
+    $(".container").append(`<svg class="plateau" height="${widthCase*9+widthBordure*8}" width="${widthCase*9+widthBordure*8}">`+generateurPlateau(widthCase,widthBordure)+`</svg>`);
+    $(".container").append(`<svg class="stockage-1" height="${widthCase*2+widthBordure}" width="${widthCase*9+widthBordure*8}">`+generateurStockage('joueur1',widthCase,widthBordure)+`</svg>`);
 
 
 
@@ -42,11 +43,13 @@ $(document).ready(function() {
 
     let tourJoueur = 1;
 
+    let compteurBatonnet = {joueur1: 7, joueur2: 7};
+
     $("body").on("click", ".case", function(){
 
 
         /* on vérifie si le joueur peut se déplacer à la case cliqué */
-        if (valideDeplacementPion($(`#joueur${tourJoueur}`).data(`ligne`),$(`#joueur${tourJoueur}`).data(`colonne`),$(this).data(`ligne`),$(this).data(`colonne`)) == true) {
+        if (validDeplacementPion($(`#joueur${tourJoueur}`).data(`ligne`),$(`#joueur${tourJoueur}`).data(`colonne`),$(this).data(`ligne`),$(this).data(`colonne`)) == true) {
             
             /* on modifie la data-contenu "vide" en "plein" si un point s'y trouve. On change également la data-contenu "plein" en vide pour la case de départ */
             $(this).data(`contenu`, "plein");
@@ -87,24 +90,32 @@ $(document).ready(function() {
     $("body").on("click", ".bordure", function(){
 
 
-        if (validePlacementBatonnet($(this).data(`contenu`),$(this).data(`ligne`),$(this).data(`colonne`),$(this).data(`ligne`),$(this).data(`colonne`)) == true) {
+        if (validPlacementBatonnet($(this).data(`contenu`),$(this).data(`ligne`),$(this).data(`colonne`),$(this).data(`ligne`),$(this).data(`colonne`)) == true) {
 
-            /* on place le batonnet sur la case cliquée */
+            /* on place le bâtonnet sur la case cliquée */
             $(this).addClass(`batonnet`);
             $(this).data(`contenu`, `plein`);
             
-            /* on prolonge le batonnet jusqu'à une bordure +1 */
+            /* on prolonge le bâtonnet jusqu'à une bordure +1 */
             placementBatonnetAdjacent($(this).data(`ligne`),$(this).data(`colonne`),$(this).attr(`width`));
 
-
+        
             /* changement de joueur */
             if (tourJoueur == 1 ) {
+                /* on enlève le bâtonnet de sa zone de stockage */
+                $(`#joueur1-${compteurBatonnet.joueur1}`).addClass('fond-stockage-batonnet');
+                
+                compteurBatonnet.joueur1 = compteurBatonnet.joueur1 - 1;
                 tourJoueur++;
             } else {
+                /* on enlève le bâtonnet de sa zone de stockage */
+                $(`#joueur2-${compteurBatonnet.joueur2}`).addClass('fond-stockage-batonnet');
+
+                compteurBatonnet.joueur2 = compteurBatonnet.joueur2 - 1;
                 tourJoueur = 1;
             }
         } else {
-            alert(`Joueur ${tourJoueur} : Il y a déjà un batonnet à cet endroit !`)
+            alert(`Joueur ${tourJoueur} : Il y a déjà un bâtonnet à cet endroit ou vous n'avez plus de bâtonnet`)
         }
 
 
@@ -145,18 +156,44 @@ $(document).ready(function() {
 
     /* Générateur de rectangle SVG selon les coordonnées et les dimensions souhaitées. Elle comprend aussi 2 données : la class CSS et les coordonnées x,y dans la propriété data */
     function generateurRect (classCss,numLigne,numColonne,positionX,positionY,largeur,hauteur) {
-        let rect = `<rect class="${classCss}" data-ligne="${numLigne}" data-colonne="${numColonne}" data-contenu="vide" x="${positionX}" y="${positionY}" width="${largeur}" height="${hauteur}"/>`;
+
+            let rect = String;
+        
+        if (classCss !== "batonnet") {
+            
+            rect = `<rect class="${classCss}" data-ligne="${numLigne}" data-colonne="${numColonne}" data-contenu="vide" x="${positionX}" y="${positionY}" width="${largeur}" height="${hauteur}"/>`;
+
+        } else {
+
+            rect = `<rect class="${classCss}" id="${numLigne}-${numColonne}" data-ligne="${numLigne}" data-colonne="${numColonne}" data-contenu="plein" x="${positionX}" y="${positionY}" width="${largeur}" height="${hauteur}"/>`;
+
+        }
+
 
         return rect;
     }
 
     /* Générateur de cercle SVG selon les coordonnées et les dimensions souhaitées. Elle comprend aussi 2 données : la class CSS du Joueur et les coordonnées x,y dans la propriété data */
     function generateurCircle (classJoueur,numLigne,numColonne,positionX,positionY,rayon) {
-        let circle = `<circle class="${classJoueur}" id="${classJoueur}" data-ligne="${numLigne}" data-colonne="${numColonne}" cx="${positionX}" cy="${positionY}" r="${rayon}" />`;
+        const circle = `<circle class="${classJoueur}" id="${classJoueur}" data-ligne="${numLigne}" data-colonne="${numColonne}" cx="${positionX}" cy="${positionY}" r="${rayon}" />`;
 
         return circle;
     }
 
+
+    function generateurStockage (joueurId, widthCase, widthBordure) {
+
+        let stockage = String;
+
+        for (let numColonne=0; numColonne<9; numColonne++) {
+            stockage = stockage+generateurRect("fond-stockage",joueurId,numColonne,numColonne*(widthCase+widthBordure),0,widthCase,widthCase*2+widthBordure);
+        }
+        for (let numColonne=0; numColonne<8; numColonne++) {
+            stockage = stockage+generateurRect("batonnet",joueurId,numColonne,widthCase+numColonne*(widthCase+widthBordure),0,widthBordure,widthCase*2+widthBordure);
+        }
+
+        return stockage;
+    }
 
     
     /* Générateur d'un plateau aux dimensions voulues */
@@ -171,7 +208,7 @@ $(document).ready(function() {
             }
             /* Génération des bordures verticales */
             for (let numColonne=0; numColonne<8; numColonne++) {
-                plateau = plateau+generateurRect("bordure",numLigne,numColonne,widthCase+numColonne*(widthCase+widthBordure),numLigne*(widthCase+widthBordure),widthBordure,widthCase);
+                plateau = plateau+generateurRect("bordure verticale",numLigne,numColonne,widthCase+numColonne*(widthCase+widthBordure),numLigne*(widthCase+widthBordure),widthBordure,widthCase);
             }
         }
 
@@ -179,7 +216,7 @@ $(document).ready(function() {
         for (let numLigne = 0; numLigne<8; numLigne++) {
             /* Génération des bordures horizontales */
             for (let numColonne=0; numColonne<9; numColonne++) {
-                plateau = plateau+generateurRect("bordure",numLigne,numColonne,numColonne*(widthCase+widthBordure),widthCase+numLigne*(widthCase+widthBordure),widthCase,widthBordure);
+                plateau = plateau+generateurRect("bordure horizontale",numLigne,numColonne,numColonne*(widthCase+widthBordure),widthCase+numLigne*(widthCase+widthBordure),widthCase,widthBordure);
             }
             /* Génération des intersections */
             for (let numColonne=0; numColonne<8; numColonne++) {
@@ -206,14 +243,16 @@ $(document).ready(function() {
 
 
 
-    function valideDeplacementPion (ligneDepart,colonneDepart,ligneArrivee,colonneArrivee) {
+    function validDeplacementPion (ligneDepart,colonneDepart,ligneArrivee,colonneArrivee) {
 
         let verifLigne = Boolean , verifColonne = Boolean
         
         verifLigne = ligneDepart == ligneArrivee;
 
-        
-        if ($(`.case[data-ligne=${ligneArrivee}] + .case[data-colonne=${colonneArrivee}]`).data(`contenu`) == "vide") {
+        /* Première condition -> vérifie si la case d'arrivée est vide
+        Deuxième condition -> vérifie si la bordure d'entre les cases est vide (BUG) */
+        if ($(`.case[data-ligne=${ligneArrivee}] + .case[data-colonne=${colonneArrivee}]`).data(`contenu`) == "vide" &&
+        $(`.bordure[data-ligne=${ligneArrivee}] + .bordure[data-colonne=${colonneArrivee}]`).data(`contenu`) == "vide") {
             if (verifLigne == true) {
                 verifColonne = (colonneDepart == colonneArrivee - 1) || (colonneDepart == colonneArrivee + 1);
             } else {
@@ -240,16 +279,29 @@ $(document).ready(function() {
 
 
 
-    function validePlacementBatonnet(contenu,ligneSelect,colonneSelect) {
-        /* on vérifie s'il n'y a pas de batonnet sur la bordure cliqué */
-        if (contenu == "vide") {
-            /* on vérifie s'il n'y a pas de batonnet sur l'intersection entre les 2 bordures */
-            if ($(`.intersection[data-ligne=${ligneSelect}] + .intersection[data-colonne=${colonneSelect}]`).data(`contenu`) == "vide") {
-                return true;
+    function validPlacementBatonnet(contenu,ligneSelect,colonneSelect) {
+        if (tourJoueur == 1) {
+            if (compteurBatonnet.joueur1 !== -1) {
+                /* on vérifie s'il n'y a pas de bâtonnet sur la bordure cliqué */
+                if (contenu == "vide") {
+                    /* on vérifie s'il n'y a pas de bâtonnet sur l'intersection entre les 2 bordures */
+                    if ($(`.intersection[data-ligne=${ligneSelect}] + .intersection[data-colonne=${colonneSelect}]`).data(`contenu`) == "vide") {
+                        return true;
+                    }
+                } else { return false }
             }
         } else {
-            return false
+            if (compteurBatonnet.joueur2 !== -1) {
+                /* on vérifie s'il n'y a pas de bâtonnet sur la bordure cliqué */
+                if (contenu == "vide") {
+                    /* on vérifie s'il n'y a pas de bâtonnet sur l'intersection entre les 2 bordures */
+                    if ($(`.intersection[data-ligne=${ligneSelect}] + .intersection[data-colonne=${colonneSelect}]`).data(`contenu`) == "vide") {
+                        return true;
+                    }
+                } else { return false }
+            }
         }
+        
     }
 
     
@@ -264,7 +316,8 @@ $(document).ready(function() {
     function placementBatonnetAdjacent(ligneSelect,colonneSelect,widthSelect) {
 
 
-        /* si la bordure cliquée est verticale alors on ajoute le batonnet sur la bordure +1 */
+
+        /* si la bordure cliquée est verticale alors on ajoute le bâtonnet sur la bordure +1 */
         if (widthSelect == widthBordure) {
             $(`.bordure[data-ligne=${ligneSelect+1}] + .bordure[width=${widthSelect}] + .bordure[data-colonne=${colonneSelect}]`).addClass(`batonnet`);
             $(`.bordure[data-ligne=${ligneSelect+1}] + .bordure[width=${widthSelect}] + .bordure[data-colonne=${colonneSelect}]`).data(`contenu`, `plein`);
